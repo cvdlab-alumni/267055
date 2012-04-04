0ab885320f90a48575a116aeb5fb35212ad4cb7e
Giulia Brunori <giu.brunori@stud.uniroma3.it>
2012-04-04 17:46:06 +0300

    Update exercise2.js

--- exercise2.js
+++ exercise2.js
@@ -1,1 +1,8 @@
 var extremeSxWall = (SIMPLEX_GRID([([6,8,6.5]),[0.5,0,0],[3,0,0]]));
+
+//Cercare di snellire
+var upWall = (SIMPLEX_GRID([
+  ([0,5,3]),[8,0,0],[4,0,0]
+]));
+
+var baseStruct = STRUCT([extremeSxWall,upWall]);
