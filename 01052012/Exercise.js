d017fc06859135119b559cceb756a731a0be8a95
Giulia Brunori <giu.brunori@stud.uniroma3.it>
2012-05-01 18:18:48 +0300

    Update 01052012/Exercise.js

--- 01052012/Exercise.js
+++ 01052012/Exercise.js
@@ -11,19 +11,15 @@ var labirint4 = POLYLINE([[3.5,0], [3.5,4.5]]);
 var labirint5 = POLYLINE([[3.5,4.5], [2,4.5]]);
 var labWall2 = POLYLINE([[2,0],[2,0.5],[3,0.5],[3,2],[2,2],[2,2.5],[3,2.5],[3,4],[2,4],[2,4.5]]);
 
-
 var up = STRUCT([labirint1,labirint3,labirint6, labWall1]);
 DRAW(up);
-
 
 var down = STRUCT([labirint2,labirint4,labirint5, labWall2]);
 DRAW(down);
 
-
 var upWall = (EXTRUDE([2]))(up);
 var upWallColored = COLOR([0.2,0.5,0.5,1])(upWall);
 DRAW(upWallColored);
-
 
 var downWall = (EXTRUDE([2]))(down);
 var downWallColored = COLOR([0.2,0.5,0.5,1])(downWall);
@@ -31,13 +27,11 @@ DRAW(downWallColored);
 
 //ESERCIZIO 2
 
-
 var roof = (BOUNDARY(CUBOID([3.5,4.5,0.5])));
 var heightRoof = T([2])([2])(roof);
 var coloredRoof = COLOR([0,1,0,0.5])(heightRoof);
 
 DRAW(coloredRoof);
-
 
 //ESERCIZIO 3
 
@@ -46,3 +40,10 @@ var controlPoints = [[1,0],[1,1],[1,0],[1,1]];
 var hermiteCubicalCurve = CUBIC_HERMITE(S0)(controlPoints);
 var curve = MAP(hermiteCubicalCurve)(domain);
 DRAW(curve);
+
+//ESERCIZIO 4 
+var bezierDomain = INTERVALS(1)(30);
+var bezierControlPoints = [[0,0],[3,1],[1,2],[2,3],[3,2]];
+var mapBezier = BEZIER(S0)(bezierControlPoints);
+var bezierCurve = MAP(mapBezier)(bezierDomain);
+DRAW(bezierCurve);
