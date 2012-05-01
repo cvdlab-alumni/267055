6e7ed43d7e7b5f4f0b19dd51c34146a7a96f8907
Giulia Brunori <giu.brunori@stud.uniroma3.it>
2012-05-01 22:51:58 +0300

    Update exercise2.js

--- exercise2.js
+++ exercise2.js
@@ -1,19 +1,17 @@
 var extremeSxWall = (SIMPLEX_GRID([([6,8,6.5]),[0.5,0,0],[3,0,0]]));
 DRAW(extremeSxWall);
 
-var sxWall = T([1])([8])(SIMPLEX_GRID([
-  ([0,5,1.5]),[0.5,0,0],[3,0,0]]));
+var sxWall = T([1])([8])(SIMPLEX_GRID([([0,5.5,1.5]),[0.5,0,0],[3,0,0]]));
 
 var extremeSxFloor = (SIMPLEX_GRID([([3,5,3]),[8,0.5,0],[0.5,0,0]]));
-var extremeSxRoof = T([2])([2.5])(extremeSxFloor);
+var extremeSxRoof = T([1,2])([0.5,2.5])(extremeSxFloor);
+var extremeSxbackWall = SIMPLEX_GRID([[0.5],[0,8],[3]]);
 
-var baseStruct = STRUCT([extremeSxWall,sxWall,extremeSxFloor,extremeSxRoof]);
+var baseStruct = STRUCT([extremeSxWall,sxWall,extremeSxFloor,extremeSxRoof, extremeSxbackWall]);
 DRAW(baseStruct);
 
-var big_pool = STRUCT([
-  T([0,0])([1,10])(SIMPLEX_GRID([[9],[19],[0.2]]))
-]);
+var big_pool = STRUCT([T([0,0])([1,10])(SIMPLEX_GRID([[8],[15],[0.2]]))]);
 DRAW(COLOR(0.2,0,0.4,0.8)(big_pool));
 
-var back = T([0,1])([7,7])(SIMPLEX_GRID([[4],[4,15],[0.5]]));
-DRAW(back);
+var backFloor = T([0,1])([7,7])(SIMPLEX_GRID([[4],[4,15],[0.5]]));
+DRAW(backFloor);
