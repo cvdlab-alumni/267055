ad32ebbaeae0066b9761173d9b396e4b998a56ac
Giulia Brunori <giu.brunori@stud.uniroma3.it>
2012-05-05 11:20:26 +0300

    Update 2012-05-04/exercise1.js

--- 2012-05-04/exercise1.js
+++ 2012-05-04/exercise1.js
@@ -6,19 +6,25 @@ var traslaPoints = function (point, zTranslation)  {
 	});
 }
 
-var scala = function (point, factor)  {
+var scala = function (point, scaleFactor)  {
   return point.map(function (p){
-		return [p[0]*factor, p[1]*factor, p[2]];
+		return [p[0]*scaleFactor, p[1]*scaleFactor, p[2]];
 	});
 }
 var p0 = [[2,1,0],[0,1,0],[0,-0.5,0], [2,0,0], [2,1,0]];
 var p1 = scala(traslaPoints(p0,1), 0.8);
 var p2 = scala(traslaPoints(p0,2), 0.5);
 
-var c0 = BEZIER(S0)(p0);
-var c1 = BEZIER(S0)(p1);
-var c2 = BEZIER(S0)(p2);
+var points = [p0, p1,p2];
 
-var ala = BEZIER(S1)([c0,c1,c2]);
+var allBezier = function(points){
+	var bezier = [ ];
+	points.forEach( function(p){
+		bezier.push(BEZIER(S0)(p));
+	});
+	return bezier;
+}
+
+var ala = BEZIER(S1)(allBezier(points));
 var surfAla = MAP(ala)(domain);
 DRAW(COLOR([255/255,204/255,0/255])(surfAla));
