a53cf0a10c543039f684a1d05416e350eb053814
Giulia Brunori <giu.brunori@stud.uniroma3.it>
2012-05-05 18:14:47 +0300

    Update 2012-05-04/exercise5.js

--- 2012-05-04/exercise5.js
+++ 2012-05-04/exercise5.js
@@ -1,7 +1,6 @@
 
-  var vgrass = CUBOID ([45, 100, 0.2]);
-	var hgrass1 = T([1])([100])(CUBOID([100, 45, 0.2]));
-	var hgrass2 = T([0,1])([-100,100])(CUBOID([100, 45, 0.2]));
-	var hgrass =STRUCT([hgrass1,hgrass2]);
-	DRAW(vgrass);
-	DRAW(hgrass);
+var vgrass = CUBOID ([45, 100, 0.2]);
+var hgrass1 = T([1])([100])(CUBOID([100, 45, 0.2]));
+var hgrass2 = T([0,1])([-100,100])(CUBOID([100, 45, 0.2]));
+var grass = COLOR([0/255, 100/255, 0/255])(STRUCT([hgrass1,hgrass2,vgrass]));
+DRAW(grass);
