f77dfe7ac61f794935786b88e0583b688e5e6e69
Giulia Brunori <giu.brunori@stud.uniroma3.it>
2012-05-05 19:31:44 +0300

    Update 2012-05-04/exercise5.js

--- 2012-05-04/exercise5.js
+++ 2012-05-04/exercise5.js
@@ -3,8 +3,9 @@ var hStreet1 = T([1])([100])(CUBOID([100, 50, 0.2]));
 var hStreet2 = T([0,1])([-60,100])(CUBOID([70, 50, 0.2]));
 var vContinuosLine1 = COLOR([255/255,204/255,0/255])(T([0])([3])(CUBOID ([2, 100, 0.2])));
 var vContinuosLine2 = COLOR([255/255,204/255,0/255])(T([0])([43])(CUBOID ([2, 100, 0.2])));
+var vContinuosLine3 = COLOR([255/255,204/255,0/255])(T([0,1])([-60,145])(CUBOID([160, 2, 0.2])));
 var street = COLOR([0.3,0.3,0.3])(STRUCT([vStreet,hStreet1,hStreet2]));
-var streetWithLine = STRUCT([street,vContinuosLine1,vContinuosLine2]);
+var streetWithLine = STRUCT([street,vContinuosLine1,vContinuosLine2,vContinuosLine3]);
 DRAW(streetWithLine);
 
 var vgrass1 = T([0])([-10])(CUBOID ([10, 100, 0.2]));
